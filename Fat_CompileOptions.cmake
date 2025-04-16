875c0d88d86287a29b3a3be826345dab15e28c36
fatpound <84704675+fatpound@users.noreply.github.com>
2025-04-17 00:35:15 +0300

    initial commit

---
+++ Fat_CompileOptions.cmake
@@ -0,0 +1,125 @@
+#[[ fatpound ]]#
+
+add_library (Fat_CompileOptions INTERFACE)
+add_library (Fat::CompileOptions ALIAS Fat_CompileOptions)
+
+if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+    target_compile_options(Fat_CompileOptions INTERFACE
+        ##################################
+        -pedantic # Conform to ISO/IEC C++
+        
+
+        ## Active warnings
+        -Wall
+        -Wextra
+        -Wpedantic
+
+
+        ## Inactive warnings
+        # -Wno-newline-eof
+
+
+        ## Preprocessor
+        -DFAT_BUILDING_WITH_MSVC=0
+        
+
+        ## Configuration-specific
+        $<$<CONFIG:Debug>:
+            -O0
+
+            -Werror
+        >
+        $<$<CONFIG:Release>:
+            -O2
+        >
+
+        ## Standard library
+        $<$<STREQUAL:${CMAKE_HOST_SYSTEM_NAME}, Linux>:
+            -stdlib=libstdc++
+        >
+    )
+
+elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "IntelLLVM")
+    target_compile_options (Fat_CompileOptions INTERFACE
+        ##################################
+        -pedantic # Conform to ISO/IEC C++
+
+        
+        ## Active warnings
+        -Weverything
+
+
+        ## Inactive warnings
+        -Wno-c++98-compat
+        -Wno-c++98-compat-pedantic
+        
+        -Wno-c++20-compat
+        -Wno-c++20-extensions
+
+        -Wno-padded
+        # -Wno-newline-eof
+
+
+        ## Preprocessor
+        -DFAT_BUILDING_WITH_MSVC=0
+
+
+        ## Configuration-specific
+        $<$<CONFIG:Debug>:
+            -O0
+            
+            -Werror
+        >
+        $<$<CONFIG:Release>:
+            -O2
+        >
+
+        ## Standard library
+        $<$<STREQUAL:${CMAKE_HOST_SYSTEM_NAME}, Linux>:
+            -stdlib=libc++
+        >
+    )
+
+elseif (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+    target_compile_options (Fat_CompileOptions INTERFACE
+        #####################################
+        /permissive- # Conform to ISO/IEC C++
+        /std:c++latest
+
+
+        ## Active warnings
+        /Wall
+
+
+        ## Inactive warnings
+        /wd4061 # Not all enum identifiers of an Enum (class) are handled by a switch statement (When there is a default case)
+        /wd4062 # Not all enum identifiers of an Enum (class) are handled by a switch statement (When there is NOT a default case)
+        /wd4324 # structure was padded due to alignment specifier
+        /wd4514 # Unreferenced inline function has been removed
+        /wd4820 # n bytes padding added after construct MyClass
+        /wd5045 # Compiler will insert Spectre mitigation for memory load if /Qspectre switch specified
+        /wd5246 # outdated warning, the initialization of a subobject should be wrapped in braces
+        
+
+        ## Preprocessor
+        -DFAT_BUILDING_WITH_MSVC=1
+
+        
+        ## Configuration-specific
+        $<$<CONFIG:Debug>:
+            /Od
+
+            /WX # Treat warnings as errors
+        >
+        $<$<CONFIG:Release>:
+            /O2
+
+            /GF # String pooling
+            /GL # Whole-program optimization
+
+            /Qvec-report:2 # Auto vectorizer reporting (max)
+            /Qpar-report:2 # Parallelizer reporting (max)
+        >
+    )
+
+endif () 
---
+++ src/Main.cxx
@@ -0,0 +1,12 @@
+import FatPound;
+
+import std;
+
+int main(
+    [[maybe_unused]] const int argc,
+    [[maybe_unused]] const char* const* const argv)
+{
+    std::println<>("hello");
+
+    return 0;
+}
